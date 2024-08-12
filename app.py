7dd605bd81bb0252a5a422cda6a82daae04540bc
Eric Mutuma <ericmutuma15@gmail.com>
2024-08-12 12:15:10 +0300

    add postgresql db configurations for deployment

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
+from config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -10,12 +12,15 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+    migrate = Migrate(app, db)
     app.config['SECRET_KEY'] = 'wewewacha'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(base_dir, "main.db")}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['UPLOAD_FOLDER'] = 'static/menu_images'
     app.config['LOGO_UPLOAD_FOLDER'] = 'static/logos'
     app.config['PIC_UPLOAD_FOLDER'] = 'static/profile_pics'
+    
 
     app.config['MAX_CONTENT_PATH'] = 16 * 1024 * 1024  # 16 MB max file size
 
